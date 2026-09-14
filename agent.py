5f5af17f06c81b0260a9473d0f4fe42def5d45ee
Adi Parvatharajan <adiparvatharajan@Adis-MacBook-Air.local>
2026-09-13 17:22:51 -0700

    basic chat loop and empty message + block text management

--- agent.py
+++ agent.py
@@ -5,14 +5,32 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 client = anthropic.Anthropic()  # Initialize the Anthropic client
-input = input("Enter message: ")
 
-response = client.messages.create(
-    model="claude-sonnet-5",
-    max_tokens=1024,
-    messages=[
-        {"role": "user", "content": input}
-    ]
-)
+messages = []
 
-print(response.content[0].text)  # Print the response from the model
+while True:
+    entry = input("Enter message: ")
+
+    #skip empty messages
+    if entry.strip() == "":
+        continue
+
+    #exit the program if the user types "exit" or "quit"
+    if entry in ["exit", "quit"]:
+        print("Exiting the program. Have a nice day!")
+        break
+
+    messages.append({"role": "user", "content": entry})  # Append user message to messages list
+
+    response = client.messages.create(
+        model="claude-sonnet-5",
+        max_tokens=1024,
+        messages=messages
+    )
+    #Skip thinking blocks and print only the text response from the model
+    for block in response.content:
+        if block.type == "text":
+            print(block.text)  # Print the response from the model
+
+    messages.append({"role": "assistant", "content": response.content})  # Append assistant response to messages list
+    
